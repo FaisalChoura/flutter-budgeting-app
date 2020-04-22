849554f68a512b5cb3b0758a89cde5816d409f34
Faisal Choura <faisal.choura@gmail.com>
2020-04-22 14:58:31 +0100

    added turn animation to down icon

--- lib/screens/month.dart
+++ lib/screens/month.dart
@@ -17,90 +17,7 @@ class MonthScreen extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            // TODO check fragmantation for entire app
-            height: MediaQuery.of(context).size.height * 0.25,
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.only(bottom: 16),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 31, 109, 255),
-                  Color.fromARGB(255, 31, 184, 255),
-                ],
-                begin: new Alignment(0.0, 0.35),
-                end: Alignment.topCenter,
-              ),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(60, 0, 0, 0),
-                  offset: Offset(0, 3),
-                  blurRadius: 6.0,
-                ),
-              ],
-            ),
-            child: Align(
-              alignment: new Alignment(0.0, 0.4),
-              child: Wrap(
-                crossAxisAlignment: WrapCrossAlignment.center,
-                direction: Axis.vertical,
-                children: <Widget>[
-                  Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 6),
-                        child: Text(
-                          '£ ',
-                          style: TextStyle(fontSize: 32, color: Colors.white),
-                        ),
-                      ),
-                      AnimatedDefaultTextStyle(
-                        duration: Duration(milliseconds: 250),
-                        style: TextStyle(
-                          fontSize: 48,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          shadows: [
-                            Shadow(
-                              color: Color.fromARGB(60, 0, 0, 0),
-                              offset: Offset(0, 3),
-                              blurRadius: 6,
-                            ),
-                          ],
-                        ),
-                        child: Text(
-                          month.amount.toStringAsFixed(2),
-                        ),
-                      ),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(50),
-                          onTap: () {
-                            print('hello');
-                          },
-                          child: Icon(
-                            Icons.keyboard_arrow_down,
-                            size: 48,
-                            color: Colors.white,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Text(
-                    'Total Spending',
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  )
-                ],
-              ),
-            ),
-          ),
+          MonthHeader(month: month),
           Expanded(
             child: GroupedListView(
               padding: EdgeInsets.all(0),
@@ -162,5 +79,147 @@ class MonthScreen extends StatelessWidget {
       suffix = <String>['st', 'nd', 'rd'][digit - 1];
     }
     return '$weekDayName $day$suffix';
+  }
+}
+
+class MonthHeader extends StatefulWidget {
+  const MonthHeader({
+    Key key,
+    @required this.month,
+  }) : super(key: key);
+
+  final Month month;
+
+  @override
+  _MonthHeaderState createState() => _MonthHeaderState();
+}
+
+class _MonthHeaderState extends State<MonthHeader>
+    with TickerProviderStateMixin {
+  bool _expanded = false;
+  double _height;
+  AnimationController turnController;
+
+  initState() {
+    turnController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _height = _expanded
+        ? MediaQuery.of(context).size.height * 0.45
+        : MediaQuery.of(context).size.height * 0.25;
+
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 250),
+      curve: Curves.easeInCubic,
+      // TODO check fragmantation for entire app
+      height: _height,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(bottom: 16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 31, 109, 255),
+            Color.fromARGB(255, 31, 184, 255),
+          ],
+          begin: new Alignment(0.0, 0.35),
+          end: Alignment.topCenter,
+        ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(60, 0, 0, 0),
+            offset: Offset(0, 3),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.11,
+            left: MediaQuery.of(context).size.width * 0.25,
+            // alignment: new Alignment(0.0, 0.4),
+            child: Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              direction: Axis.vertical,
+              children: <Widget>[
+                Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 6),
+                      child: Text(
+                        '£ ',
+                        style: TextStyle(fontSize: 32, color: Colors.white),
+                      ),
+                    ),
+                    AnimatedDefaultTextStyle(
+                      duration: Duration(milliseconds: 250),
+                      style: TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            color: Color.fromARGB(60, 0, 0, 0),
+                            offset: Offset(0, 3),
+                            blurRadius: 6,
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        widget.month.amount.toStringAsFixed(2),
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(50),
+                        onTap: () {
+                          _toggleExpansion();
+                        },
+                        child: RotationTransition(
+                          turns: Tween(begin: 0.0, end: 0.5)
+                              .animate(turnController),
+                          child: Icon(
+                            Icons.keyboard_arrow_down,
+                            size: 48,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Text(
+                  'Total Spending',
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _toggleExpansion() {
+    setState(() {
+      _expanded = !_expanded;
+      if (_expanded) {
+        turnController.forward();
+      } else {
+        turnController.reverse();
+      }
+    });
   }
 }
