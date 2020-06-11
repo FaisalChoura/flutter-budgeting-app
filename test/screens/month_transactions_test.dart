87b6ca4b6cbec57f17a623c3f1cd5aa41dc4be4a
Faisal Choura <faisal.choura@gmail.com>
2020-06-11 10:47:38 +0100

    + commented code to validate transaction deletetion

--- test/screens/month_transactions_test.dart
+++ test/screens/month_transactions_test.dart
@@ -9,14 +9,15 @@ import 'package:provider/provider.dart';
 import '../helpers/mocks.dart';
 import '../helpers/stubs.dart';
 
-// TODO how to test item being removed after delete firebase
+// TODO what's the best way to test firestore refreshes
+// Look into this https://pub.dev/packages/cloud_firestore_mocks
 
 void main() {
   group("Month Screen", () {
     testWidgets("Displays correct information", (WidgetTester tester) async {
       final transactionsService = TransactionsServiceMock();
       final user = FirebaseUserMock();
-
+      stubTransactionsPerMonth(transactionsService, user, 2020, 10);
       await pumpMonthScreen(tester, transactionsService, user);
       expect(find.text("October"), findsOneWidget);
       expect(find.text("70.00"), findsOneWidget);
@@ -25,15 +26,20 @@ void main() {
     testWidgets("Menu button and Delete ", (WidgetTester tester) async {
       final transactionsService = TransactionsServiceMock();
       final user = FirebaseUserMock();
+      stubTransactionsPerMonth(transactionsService, user, 2020, 10);
 
       await pumpMonthScreen(tester, transactionsService, user);
 
       await tester.tap(find.byKey(Key("1transactionPopupMenu")));
       await tester.pumpAndSettle();
-      stubDeletedTransactionsPerMonth(transactionsService, user, 2020, 10);
       await tester.tap(find.text("Delete"));
+      transactionsService.streamTransactionsPerMonth(user, 2020, 10);
       verify(transactionsService.deleteTransaction(argThat(isA<String>())))
           .called(1);
+
+      // stubDeletedTransactionsPerMonth(transactionsService, user, 2020, 10);
+      // await pumpMonthScreen(tester, transactionsService, user);
+      // expect(find.text("KFC"), findsNothing);
     });
   });
 }
@@ -41,7 +47,6 @@ void main() {
 pumpMonthScreen(WidgetTester tester, TransactionsService transactionsService,
     FirebaseUser user) async {
   stubUserId(user);
-  stubTransactionsPerMonth(transactionsService, user, 2020, 10);
   final mockProviders = providersWithStubbedValues(transactionsService, user);
   await tester.pumpWidget(
     MultiProvider(
